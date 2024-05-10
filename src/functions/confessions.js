705d9bc0eb8509ebb05e3281e2f8cad88d42e3c2
kapilkumar7 <kapil.kumar@smit.fit>
2024-05-10 15:27:20 +0530

    chore: Add MongoDB dependency and update confession submission endpoint

--- src/functions/confessions.js
+++ src/functions/confessions.js
@@ -1,13 +1,59 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const { MongoClient } = require("mongodb");
 
-app.http('confessions', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+// Confession submission endpoint
+app.http("confessions", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    try {
+      const name = request.query.get("confession") || (await request.text());
 
-        return { body: `Hello, ${name}!` };
+      let confessionobj = {
+        confession: name,
+      };
+
+      if (!confessionobj) {
+        return { status: 400, body: "Please provide a confession." };
+      }
+
+      await client.connect();
+      const database = client.db("confessionsdb");
+      const collection = database.collection("confessions");
+
+      await collection.insertOne(confessionobj);
+
+      return { status: 200, body: "Confession submitted successfully." };
+    } catch (error) {
+      console.error("Error occurred while submitting confession:", error);
+      return { status: 500, body: "Internal Server Error" };
+    } finally {
+      await client.close();
     }
+  },
+});
+
+app.http("getAllConfessions", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async () => {
+    try {
+      await client.connect();
+      const database = client.db("confessionsdb");
+      const collection = database.collection("confessions");
+      const confessions = await collection.find({}).toArray();
+      return { status: 200, body: JSON.stringify(confessions) };
+    } catch (error) {
+      console.error("Error occurred while fetching confessions:", error);
+      return {
+        status: 500,
+        body: "An error occurred while fetching confessions.",
+      };
+    } finally {
+      await client.close();
+    }
+  },
 });
